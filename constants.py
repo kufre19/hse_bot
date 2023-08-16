543c89f4d60b8fab4e3e845876c146feadf8bd99
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-08-16 11:17:37 +0100

    added ignore

--- constants.py
+++
@@ -1,4 +0,0 @@
-# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
-# and rename this file to constants.py.
-APIKEY = "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"
-
